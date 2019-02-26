bc455364fb9a2167ded986626f65ccb7df0a1e63
Ferran <feranperez@gmail.com>
2019-02-26 19:41:27 +0100

    vfe

--- main_prova.py
+++ main_prova.py
@@ -1,9 +1,17 @@
 import logging
 import datetime
-
+import os
 from mail_manager import utils
 from mail_manager.database import Database, DatabaseConfiguration
 from mail_manager.email import Email
 from mail_manager.exceptions import MailManagerException
 
-utils.load_email("emailDB","message1")
+
+email_extension = ".txt"
+email_dir = "EmailDB"
+email_id = "message8"
+
+with open(os.path.join(email_dir,email_id+email_extension),"r") as f:
+    content = f.read().splitlines()
+    for linea in content:
+        print(linea)
