7550d52a9d932bc50328673d3a4c7fbbea6268ab
Ferran <feranperez@gmail.com>
2019-02-28 17:20:22 +0100

    Update main_prova.py

--- main_prova.py
+++ main_prova.py
@@ -9,31 +9,44 @@ from mail_manager.exceptions import MailManagerException
 
 email_extension = ".txt"
 email_dir = "EmailDB"
-email_id = "message8"
+email_id = "message1"
 
 
 def check_for_data(content):
-    data = ["Date:","From:","To:","Message_ID:","Subject:"]
+
+    data = ["Message-ID:","From:","To:","Subject:","Date:"]
+    body = [[]]
+    text = []
     filled = [None,None,None,None,None]
+    changed = False
+
     for linea in content:
         split = linea.split()
-        if (sum(1 for i in range(len(filled)) if filled[i] == None) != 0):
+        if (sum(1 for i in range(len(filled)) if filled[i] == None) != 0 and not changed):
             for i in range(len(data)): 
-                if data[i] in split:
-                    filled[i] == split[:data]
+                if data[i] == split[0]:
+                    split = split[1:]
+                    filled[i] =  " ".join(split)
                     break
+
+        elif split == [] and not changed:
+            changed = True
+
         else:
-            if split == "\n":
-                pass
-                #body = content[:el head]
-                #filled.append(body)
+            split.append(body)
+            if split != []:
+                body +="\n"
 
-                
-
-
+    for line in body:
+        text.append(line)
+    filled.append(text)
+    return filled
 
 with open(os.path.join(email_dir,email_id+email_extension),"r") as f:
     content = f.read().splitlines()
-    check_for_data(content)
+    email = check_for_data(content)
+    new_email = Email(email[0],email[1],email[2],email[3],email[4],email[5])
+    print(new_email)
 
+    #def __init__(self, email_id=None, sender=None, receiver=None, subject=None, date=None, body=None):
 
