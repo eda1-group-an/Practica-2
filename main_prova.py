4ee6bc730e088c348811b08e72b795b3e1efa70c
Ferran <feranperez@gmail.com>
2019-02-28 13:28:00 +0100

    Updated

--- main_prova.py
+++ main_prova.py
@@ -11,7 +11,29 @@ email_extension = ".txt"
 email_dir = "EmailDB"
 email_id = "message8"
 
+
+def check_for_data(content):
+    data = ["Date:","From:","To:","Message_ID:","Subject:"]
+    filled = [None,None,None,None,None]
+    for linea in content:
+        split = linea.split()
+        if (sum(1 for i in range(len(filled)) if filled[i] == None) != 0):
+            for i in range(len(data)): 
+                if data[i] in split:
+                    filled[i] == split[:data]
+                    break
+        else:
+            if split == "\n":
+                pass
+                #body = content[:el head]
+                #filled.append(body)
+
+                
+
+
+
 with open(os.path.join(email_dir,email_id+email_extension),"r") as f:
     content = f.read().splitlines()
-    for linea in content:
-        print(linea)
+    check_for_data(content)
+
+
