625a1f8095a84ecce0672ff48c5ab2fc63d3d002
Ferran <feranperez@gmail.com>
2019-02-28 17:50:48 +0100

    changes

--- main_prova.py
+++ main_prova.py
@@ -9,44 +9,42 @@ from mail_manager.exceptions import MailManagerException
 
 email_extension = ".txt"
 email_dir = "EmailDB"
-email_id = "message1"
+email_id = "CAFdmR09@mail.gmail.com"
 
-
-def check_for_data(content):
-
-    data = ["Message-ID:","From:","To:","Subject:","Date:"]
-    body = [[]]
-    text = []
-    filled = [None,None,None,None,None]
-    changed = False
-
-    for linea in content:
-        split = linea.split()
-        if (sum(1 for i in range(len(filled)) if filled[i] == None) != 0 and not changed):
-            for i in range(len(data)): 
-                if data[i] == split[0]:
-                    split = split[1:]
-                    filled[i] =  " ".join(split)
-                    break
-
-        elif split == [] and not changed:
-            changed = True
-
-        else:
-            split.append(body)
-            if split != []:
-                body +="\n"
-
-    for line in body:
-        text.append(line)
-    filled.append(text)
-    return filled
 
 with open(os.path.join(email_dir,email_id+email_extension),"r") as f:
-    content = f.read().splitlines()
-    email = check_for_data(content)
-    new_email = Email(email[0],email[1],email[2],email[3],email[4],email[5])
-    print(new_email)
 
-    #def __init__(self, email_id=None, sender=None, receiver=None, subject=None, date=None, body=None):
+    content = f.read().splitlines()
+
+    data = ["Message-ID:","From:","To:","Subject:","Date:"]
+    body = ""
+    filled = [None]*5
+    head = True #Controls if the head is over
+
+    for line in content: #We look at every line of the file
+        if (sum(1 for i in range(len(filled)) if filled[i] == None) != 0 and head): #It checks if there's data not filled and whether we are on the head or not
+            for i in range(len(data)): 
+                if data[i] in line:
+                    counter = 0
+                    for character in line:
+                        counter += 1
+                        if character == " ":
+                            break
+                    filled[i] = line[counter:]
+                    break
+
+        elif line == "" and head:
+            head = False
+
+        else:
+            body += line
+            if body != "":
+                body += "\n"
+
+    filled.append(body)    
+    new_email = Email(filled[0],filled[1],filled[2],filled[3],filled[4],filled[5])
+
+
+print(new_email)
+
 
